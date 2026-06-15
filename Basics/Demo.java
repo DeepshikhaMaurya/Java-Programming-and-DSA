653771f7bfa9f7974addea8ed4685ec602eab734
Deepshikha <deepshikhamaurya786@gamil.com>
2026-06-15 19:12:44 +0530

    Add multiple files in MyFolder

---
+++ Basics/Demo.java
@@ -0,0 +1,12 @@
+package Basics;
+public class Demo {
+ public static void main(String[] args) {
+    int age = 16;
+    if(age >= 18) {
+        System.out.println("Adult, Vote");
+    }
+    else{
+        System.out.println("Not Adult");
+    }
+ }   
+}
