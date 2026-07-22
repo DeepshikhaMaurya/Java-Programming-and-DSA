344e466ca2aacbb73b3b33ac3b8f76ab0ba5145c
Deepshikha <deepshikhamaurya786@gmail.com>
2026-07-22 22:15:20 +0530

    Added Constructors.java file

---
+++ OOPS/Constructors.java
@@ -0,0 +1,17 @@
+package OOPS;
+
+public class Constructors {
+    public static void main(String[] args) {
+        Student s1 = new Student("Shikha");
+        System.out.println(s1.name);
+    }
+}
+
+class Student {
+     String name;
+     int roll;
+
+     Student(String name) {
+         this.name  = name;
+     }
+}
