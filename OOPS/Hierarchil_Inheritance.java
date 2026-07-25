b39ba7592cb914fb8452d5877e7ddd3d9a8a0e0e
Deepshikha <deepshikhamaurya786@gmail.com>
2026-07-25 13:37:53 +0530

    Added Hierarchil_Inheritance.java file

---
+++ OOPS/Hierarchil_Inheritance.java
@@ -0,0 +1,52 @@
+package OOPS;
+
+public class Hierarchil_Inheritance {
+    public static void main(String[] args) {
+
+       Dog dobby = new Dog();
+       dobby.eat();
+       dobby.walk();
+
+       dobby.legs = 4;
+       System.out.println(dobby.legs);
+
+    }
+}
+
+//Base class
+class Animal {
+    String color;
+
+    void eat() {
+        System.out.println("eats");
+    }
+
+    void breathe() {
+        System.out.println("breathes");
+    }
+}
+
+class Mammal extends Animal {
+    void walk() {
+        System.out.println("walks");
+    }
+}
+
+
+class Dog extends Mammal {
+    int legs;
+}
+
+class Fish extends Animal {
+    void swim() {
+        System.out.println("swim");
+    }
+}
+
+class Bird extends Animal {
+    void fly() {
+        System.out.println("fly");
+    }
+}
+
+
