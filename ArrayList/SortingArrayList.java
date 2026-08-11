c48e3d257776330598bb8b58a8c5acd31b49b686
Deepshikha <deepshikhamaurya786@gmail.com>
2026-08-11 18:29:41 +0530

    Added SortingArrayList.java file

---
+++ ArrayList/SortingArrayList.java
@@ -0,0 +1,31 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class SortingArrayList {
+    public static void swap(ArrayList<Integer> list, int idx1, int idx2) {
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+    }
+     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(4);
+        list.add(7);
+        
+        System.out.println(list);
+
+        //Ascending order
+        Collections.sort(list); 
+        System.out.println(list);
+
+        //Descending order
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println(list);
+}
+}
