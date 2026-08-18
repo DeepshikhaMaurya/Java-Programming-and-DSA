2ac6b7750ab4943f3a0ad6b3cfdf5f11ca91605a
Deepshikha <deepshikhamaurya786@gmail.com>
2026-08-18 12:00:33 +0530

    Added RemoveDuplicates.java file

---
+++ Recursion_2nd_Part/RemoveDuplicates.java
@@ -0,0 +1,24 @@
+package Recursion_2nd_Part;
+
+public class RemoveDuplicates {
+    
+   public static void RemoveDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+    if(idx == str.length()) {
+        System.out.println(newStr);
+        return;
+    }
+
+    char currChar  = str.charAt(idx);
+    if(map[currChar-'a'] == true) {
+        //Duplicate
+        RemoveDuplicates(str, idx+1, newStr, map);
+    }else{
+        map[currChar-'a'] = true;
+        RemoveDuplicates(str, idx+1, newStr.append(currChar), map);
+    }
+    }
+    public static void main(String args[]) {
+        String str = "deepshikhamaurya";
+        RemoveDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+    }
+}
