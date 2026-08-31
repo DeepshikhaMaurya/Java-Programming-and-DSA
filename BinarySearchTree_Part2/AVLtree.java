15a0998860fdaa6c31d0dd127746756bbdcd2715
Deepshikha <deepshikhamaurya786@gmail.com>
2026-08-31 10:03:30 +0530

    Added AVLtree.java file

---
+++ BinarySearchTree_Part2/AVLtree.java
@@ -0,0 +1,164 @@
+package BinarySearchTree_Part2;
+
+public class AVLtree {
+
+    static class Node {
+        int data;
+        int height;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.height = 1;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node root;
+
+    // Get height of node
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return root.height;
+    }
+
+    // Get maximum
+    public static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    // Right Rotate
+    public static Node rightRotate(Node y) {
+
+        Node x = y.left;
+        Node T2 = x.right;
+
+        // Perform rotation
+        x.right = y;
+        y.left = T2;
+
+        // Update heights
+        y.height = max(height(y.left), height(y.right)) + 1;
+        x.height = max(height(x.left), height(x.right)) + 1;
+
+        // Return new root
+        return x;
+    }
+
+    // Left Rotate
+    public static Node leftRotate(Node x) {
+
+        Node y = x.right;
+        Node T2 = y.left;
+
+        // Perform rotation
+        y.left = x;
+        x.right = T2;
+
+        // Update heights
+        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(height(y.left), height(y.right)) + 1;
+
+        // Return new root
+        return y;
+    }
+
+    // Get Balance Factor
+    public static int getBalance(Node root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        return height(root.left) - height(root.right);
+    }
+
+    // Insert Node
+    public static Node insert(Node root, int key) {
+
+        // Normal BST insertion
+        if (root == null) {
+            return new Node(key);
+        }
+
+        if (key < root.data) {
+            root.left = insert(root.left, key);
+        }
+
+        else if (key > root.data) {
+            root.right = insert(root.right, key);
+        }
+
+        else {
+            // Duplicate keys not allowed
+            return root;
+        }
+
+        // Update height
+        root.height =
+                1 + max(height(root.left), height(root.right));
+
+        // Get balance factor
+        int bf = getBalance(root);
+
+        // Left Left Case
+        if (bf > 1 && key < root.left.data) {
+            return rightRotate(root);
+        }
+
+        // Right Right Case
+        if (bf < -1 && key > root.right.data) {
+            return leftRotate(root);
+        }
+
+        // Left Right Case
+        if (bf > 1 && key > root.left.data) {
+
+            root.left = leftRotate(root.left);
+
+            return rightRotate(root);
+        }
+
+        // Right Left Case
+        if (bf < -1 && key < root.right.data) {
+
+            root.right = rightRotate(root.right);
+
+            return leftRotate(root);
+        }
+
+        // Return unchanged root
+        return root;
+    }
+
+    // Preorder Traversal
+    public static void preorder(Node root) {
+
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        root = insert(root, 10);
+        root = insert(root, 20);
+        root = insert(root, 30);
+        root = insert(root, 40);
+        root = insert(root, 50);
+        root = insert(root, 25);
+
+        System.out.print("Preorder: ");
+        preorder(root);
+    }
+}
